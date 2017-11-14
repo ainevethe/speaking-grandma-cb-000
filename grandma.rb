044edbeb752e5665473a25fe296f15ab0c1933d1
Crista Cooper <aine3d@gmail.com>
2017-11-14 22:40:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
 
   winner = "I LOVE YOU GRANDMA!"
 
-  if phrase == winner
+  if phrase == winner.upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != winner.upcase
     puts "HUH? SPEAK UP, SONNY!"
